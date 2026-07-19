fdff0c44e488e79945969fb50f2054b8091d982f
NguyenTrg <truongnguyen11107@gmail.com>
2026-07-19 23:09:50 +0700

    Add .gitignore

--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/GiangsCoffee.dir/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/.qt/rcc/qrc_qmake_GiangsCoffee.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/GiangsCoffee.dir/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/.qt/rcc/qrc_qmake_GiangsCoffee.cpp.obj: \
- E:\code\Project\OOP_Coffee\GiangsCoffee\build\Desktop_Qt_6_11_1_MinGW_64_bit_Debug\.qt\rcc\qrc_qmake_GiangsCoffee.cpp
--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/GiangsCoffee_qmllint_module.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/GiangsCoffee_qmllint_module"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/GiangsCoffee_qmllint_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
