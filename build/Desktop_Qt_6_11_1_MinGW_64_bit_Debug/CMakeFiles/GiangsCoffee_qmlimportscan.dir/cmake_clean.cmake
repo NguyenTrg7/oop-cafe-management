1667fbb718e91041616e442c7b43f541a6063baf
NguyenTrg <truongnguyen11107@gmail.com>
2026-07-26 10:28:02 +0700

    Fix bug coffee system

--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/GiangsCoffee_qmlimportscan.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  ".qt/qml_imports/GiangsCoffee_build.cmake"
-  "CMakeFiles/GiangsCoffee_qmlimportscan"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/GiangsCoffee_qmlimportscan.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
